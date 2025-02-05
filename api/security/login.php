4f77cff156ff0f1f5e62291eb75180d97d6ff894
nauwai <blanquartnoe@gmail.com>
2025-02-05 12:38:21 +0100

    VARDUMP SESSION

--- api/security/login.php
+++ api/security/login.php
@@ -6,14 +6,13 @@ session_start();
 $password = $_POST["password"];
 $pseudo = $_POST["pseudo"];
 
-
 $connexion = sql_select('MEMBRE', "*", "pseudoMemb = '$pseudo'");
 
 var_dump($connexion);
 
 if ($connexion) {
     $_SESSION['id'] = $connexion[0]['numMemb'];
-    header('Location: ../../');
+    header('Location: ../../index.php');
     $_SESSION['pseudo'] = $connexion[0]['pseudoMemb'];
     $_SESSION['statut'] = $connexion[0]['numStat'];
     $_SESSION['flash']['danger'] = 'Vous êtes connecté';
